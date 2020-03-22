88342dd726fa634736b38bd9936ebd1501652baa
swappad <luca.krueger@me.com>
2020-03-22 22:32:42 +0800

    test

--- zynq/python/check-data.py
+++ zynq/python/check-data.py
@@ -4,7 +4,7 @@ from mnist import getDataSet, parse_args
 test_data, test_label = getDataSet(parse_args(), 'test')
 
 print(np.shape(test_data))
-
+# Hallo test
 non_zero = np.count_nonzero(test_data)
 
 print(non_zero)
