0e180af576ccde213a362e958191840c19af128f
swappad <luca.krueger@me.com>
2020-03-22 21:40:38 +0800

    parameters optimized to less than 8 percent misprediction

---
+++ zynq/python/check-data.py
@@ -0,0 +1,14 @@
+import numpy as np
+from mnist import getDataSet, parse_args
+
+test_data, test_label = getDataSet(parse_args(), 'test')
+
+print(np.shape(test_data))
+
+non_zero = np.count_nonzero(test_data)
+
+print(non_zero)
+
+print(np.amax(test_data))
+
+
